2eeb3e501b15de85990609cc55fdf1dfb5aac0e9
Xudong Zheng <7pkvm5aw@slicealias.com>
2022-09-13 00:54:04 -0400

    slicemk: handle reset behavior on central (2024-06-27)

--- app/src/behaviors/behavior_reset.c
+++ app/src/behaviors/behavior_reset.c
@@ -37,7 +37,7 @@ static int on_keymap_binding_pressed(struct zmk_behavior_binding *binding,
 
 static const struct behavior_driver_api behavior_reset_driver_api = {
     .binding_pressed = on_keymap_binding_pressed,
-    .locality = BEHAVIOR_LOCALITY_EVENT_SOURCE,
+    .locality = BEHAVIOR_LOCALITY_CENTRAL,
 #if IS_ENABLED(CONFIG_ZMK_BEHAVIOR_METADATA)
     .get_parameter_metadata = zmk_behavior_get_empty_param_metadata,
 #endif // IS_ENABLED(CONFIG_ZMK_BEHAVIOR_METADATA)
